c0e0ce757669881a659a7f120639a65f4cfc4c98
郭利 <18811758698@163.com>
2020-07-23 21:27:04 +0800

    add html book

--- src/books/main/main.component.ts
+++ src/books/main/main.component.ts
@@ -11,7 +11,8 @@ export class MainComponent implements OnInit {
     { name: 'Javascript', subtitle: 'Js、Es6', specialNote: 'No.1', color: '#3498db', route: 'javascript' },
     { name: 'Css', subtitle: 'css、css3', specialNote: 'No.2', color: '#f1c40f', route: 'css' },
     { name: 'Git', subtitle: 'Git常用命令', specialNote: 'No.3', color: '#16a085', route: 'git' },
-    { name: 'Typescript', subtitle: '类型声明、检查', specialNote: 'No.4', color: '#73ABFE', route: 'typescript' }
+    { name: 'Typescript', subtitle: '类型声明、检查', specialNote: 'No.4', color: '#73ABFE', route: 'typescript' },
+    { name: 'Html', subtitle: 'Html, Html5', specialNote: 'No.5', color: '#A084F1', route: 'html' }
   ]
   constructor(
     private route: Router
