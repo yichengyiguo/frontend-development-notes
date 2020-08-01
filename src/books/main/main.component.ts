0007d1789d8f288a2da41379d5c48be835462994
郭利 <18811758698@163.com>
2020-08-01 23:48:39 +0800

    update book No.

--- src/books/main/main.component.ts
+++ src/books/main/main.component.ts
@@ -8,15 +8,16 @@ import { Router } from '@angular/router'
 })
 export class MainComponent implements OnInit {
 
-  private bookIndex: number = 0;
+  private bookIndex: number = 1;
 
   bookDataArr: object[] = [
     { name: 'Javascript', subtitle: 'Js、Es6', specialNote: this.getBookNum(), color: '#3498db', route: 'javascript' },
-    { name: 'Css', subtitle: 'css、css3', specialNote: this.getBookNum(), color: '#f1c40f', route: 'css' },
-    { name: 'Git', subtitle: 'Git常用命令', specialNote: this.getBookNum(), color: '#16a085', route: 'git' },
-    { name: 'Typescript', subtitle: '类型声明、检查', specialNote: this.getBookNum(), color: '#73ABFE', route: 'typescript' },
     { name: 'Html', subtitle: 'Html、Html5', specialNote: this.getBookNum(), color: '#A084F1', route: 'html' },
-    { name: 'Angular', subtitle: '基础、进阶', specialNote: this.getBookNum(), color: '#144BA0', route: 'angular' }
+    { name: 'Css', subtitle: 'css、css3', specialNote: this.getBookNum(), color: '#f1c40f', route: 'css' },
+    { name: 'Angular', subtitle: '基础、进阶', specialNote: this.getBookNum(), color: '#144BA0', route: 'angular' },
+    { name: 'Typescript', subtitle: '类型声明、检查', specialNote: this.getBookNum(), color: '#73ABFE', route: 'typescript' },
+    { name: 'Git', subtitle: 'Git常用命令', specialNote: this.getBookNum(), color: '#16a085', route: 'git' },
+    { name: 'Typescript', subtitle: '类型声明、检查', specialNote: this.getBookNum(), color: '#73ABFE', route: 'typescript' }
   ]
   constructor(
     private route: Router
